1afd6586549ff294abadbbb223ae87a7205a2787
Alex Martens <alex@thinglab.org>
2022-09-26 17:35:28 -0700

    flake: add aarch64 builds

--- flake.nix
+++ flake.nix
@@ -16,7 +16,11 @@
     crane,
     flake-utils,
   }:
-    flake-utils.lib.eachSystem ["x86_64-linux"] (system: let
+    flake-utils.lib.eachSystem [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-linux"
+    ] (system: let
       pkgs = nixpkgs.legacyPackages.${system};
       craneLib = crane.lib.${system};
 
