0373de08918d5b7a6535e80976a39ac2234551ee
Alex Martens <alex@thinglab.org>
2022-11-11 08:33:05 -0800

    flake.lock: update
    • Updated input 'crane':
        'github:ipetkov/crane/b70e77d2e2d480a3a0bce3ecd2d981679588b23f' (2022-11-04)
      → 'github:ipetkov/crane/074825a9e8d6446564e2ae6949ac3feb79aa7397' (2022-11-10)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/636051e353461f073ac55d5d42c1ed062a345046' (2022-11-02)
      → 'github:NixOS/nixpkgs/872fceeed60ae6b7766cc0a4cd5bf5901b9098ec' (2022-11-09)

--- flake.nix
+++ flake.nix
@@ -4,11 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-    };
+
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
+    crane.inputs.flake-utils.follows = "flake-utils";
   };
 
   outputs = {
